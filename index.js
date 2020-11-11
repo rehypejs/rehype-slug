040f01cb83516ac0c4b4a40afd1291366c48e347
Titus Wormer <tituswormer@gmail.com>
2020-11-11 09:41:31 +0100

    Refactor to improve bundle size

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 'use strict'
 
 var slugs = require('github-slugger')()
-var visit = require('unist-util-visit')
-var toString = require('hast-util-to-string')
-var is = require('hast-util-is-element')
 var has = require('hast-util-has-property')
+var rank = require('hast-util-heading-rank')
+var toString = require('hast-util-to-string')
+var visit = require('unist-util-visit')
 
 module.exports = slug
-
-var headings = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
 
 function slug() {
   return transformer
@@ -18,7 +16,7 @@ function transformer(tree) {
   slugs.reset()
 
   visit(tree, 'element', function (node) {
-    if (is(node, headings) && !has(node, 'id')) {
+    if (rank(node) && !has(node, 'id')) {
       node.properties.id = slugs.slug(toString(node))
     }
   })
