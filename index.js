4e8a6817eb757e4568155d0ddda0cf4caecdf495
Titus Wormer <tituswormer@gmail.com>
2021-08-02 09:08:32 +0200

    Update dependencies

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import Slugger from 'github-slugger'
-import has from 'hast-util-has-property'
-import rank from 'hast-util-heading-rank'
-import toString from 'hast-util-to-string'
-import visit from 'unist-util-visit'
+import {hasProperty} from 'hast-util-has-property'
+import {headingRank} from 'hast-util-heading-rank'
+import {toString} from 'hast-util-to-string'
+import {visit} from 'unist-util-visit'
 
 const slugs = new Slugger()
 
@@ -14,7 +14,7 @@ function transformer(tree) {
   slugs.reset()
 
   visit(tree, 'element', function (node) {
-    if (rank(node) && !has(node, 'id')) {
+    if (headingRank(node) && !hasProperty(node, 'id')) {
       node.properties.id = slugs.slug(toString(node))
     }
   })
