5be8f0815c4a3d4cedf875a0b55150ca87de9dd8
Adam Grohs <adam@uniquelyparticular.com>
2023-03-11 11:08:10 -0500

    feat: added new pluggable Slugger

--- index.js
+++ index.js
@@ -3,10 +3,35 @@
  */
 
 /**
+ * @typedef {Function} SlugFunction
+ *   Custom `slug` implementation (optional).
+ * @param  {string} value
+ *   String of text to 'slugify'
+ * @return {string}
+ *   A unique slug string
+ */
+
+/**
+ * @typedef {Function} ResetFunction
+ *   Custom `reset` implementation (optional).
+ */
+
+/**
+ * @typedef SluggerImplementation
+ *   Custom `Slugger` implementation (optional).
+ * @property {SlugFunction} slug
+ *   The function to call for `slug(string)`
+ * @property {ResetFunction} [reset]
+ *   The function to call for `reset()`
+ */
+
+/**
  * @typedef Options
  *   Configuration (optional).
  * @property {string} [prefix='']
  *   Prefix to add in front of `id`s.
+ * @property {SluggerImplementation} [slugger]
+ *  The `Slugger` implementation to use.
  */
 
 import Slugger from 'github-slugger'
@@ -24,13 +49,18 @@ const slugs = new Slugger()
  */
 export default function rehypeSlug(options = {}) {
   const prefix = options.prefix || ''
+  const slugger = options.slugger || slugs
 
   return (tree) => {
-    slugs.reset()
+    if (slugger.reset) {
+      slugger.reset()
+    }
 
     visit(tree, 'element', (node) => {
       if (headingRank(node) && node.properties && !hasProperty(node, 'id')) {
-        node.properties.id = prefix + slugs.slug(toString(node))
+        const hmm = toString(node)
+        console.log(`>>> > visit > hmm:`, hmm)
+        node.properties.id = prefix + slugger.slug(toString(node))
       }
     })
   }
--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 import test from 'tape'
+import slugify from 'slugify'
 import {rehype} from 'rehype'
 import rehypeSlug from './index.js'
 
 test('rehypeSlug', (t) => {
-  t.plan(4)
+  t.plan(10)
 
   rehype()
     .data('settings', {fragment: true})
@@ -15,7 +16,8 @@ test('rehypeSlug', (t) => {
         '  <h2>dolor—sit—amet</h2>',
         '  <h3>consectetur &amp; adipisicing</h3>',
         '  <h4>elit</h4>',
-        '  <h5>elit</h5>',
+        '  <h5>Elit</h5>',
+        '  <h5>Elit </h5>',
         '  <p>sed</p>',
         '</section>'
       ].join('\n'),
@@ -30,7 +32,8 @@ test('rehypeSlug', (t) => {
             '  <h2 id="dolorsitamet">dolor—sit—amet</h2>',
             '  <h3 id="consectetur--adipisicing">consectetur &#x26; adipisicing</h3>',
             '  <h4 id="elit">elit</h4>',
-            '  <h5 id="elit-1">elit</h5>',
+            '  <h5 id="elit-1">Elit</h5>',
+            '  <h5 id="elit-">Elit </h5>',
             '  <p>sed</p>',
             '</section>'
           ].join('\n'),
@@ -41,7 +44,7 @@ test('rehypeSlug', (t) => {
 
   rehype()
     .data('settings', {fragment: true})
-    .use(rehypeSlug, {prefix: 'test-'})
+    .use(rehypeSlug, {prefix: 'prefix--'})
     .process(
       [
         '<section>',
@@ -49,7 +52,8 @@ test('rehypeSlug', (t) => {
         '  <h2>dolor—sit—amet</h2>',
         '  <h3>consectetur &amp; adipisicing</h3>',
         '  <h4>elit</h4>',
-        '  <h5>elit</h5>',
+        '  <h5>Elit</h5>',
+        '  <h5>Elit </h5>',
         '  <p>sed</p>',
         '</section>'
       ].join('\n'),
@@ -60,11 +64,134 @@ test('rehypeSlug', (t) => {
           String(file),
           [
             '<section>',
-            '  <h1 id="test-lorem-ipsum-">Lorem ipsum 😪</h1>',
-            '  <h2 id="test-dolorsitamet">dolor—sit—amet</h2>',
-            '  <h3 id="test-consectetur--adipisicing">consectetur &#x26; adipisicing</h3>',
-            '  <h4 id="test-elit">elit</h4>',
-            '  <h5 id="test-elit-1">elit</h5>',
+            '  <h1 id="prefix--lorem-ipsum-">Lorem ipsum 😪</h1>',
+            '  <h2 id="prefix--dolorsitamet">dolor—sit—amet</h2>',
+            '  <h3 id="prefix--consectetur--adipisicing">consectetur &#x26; adipisicing</h3>',
+            '  <h4 id="prefix--elit">elit</h4>',
+            '  <h5 id="prefix--elit-1">Elit</h5>',
+            '  <h5 id="prefix--elit-">Elit </h5>',
+            '  <p>sed</p>',
+            '</section>'
+          ].join('\n'),
+          'should match'
+        )
+      }
+    )
+
+  rehype()
+    .data('settings', {fragment: true})
+    .use(rehypeSlug, {
+      slugger: {slug: (_value) => `test--${_value}--test`}
+    })
+    .process(
+      [
+        '<section>',
+        '  <h1>Lorem ipsum 😪</h1>',
+        '  <h2>dolor—sit—amet</h2>',
+        '  <h3>consectetur &amp; adipisicing</h3>',
+        '  <h4>elit</h4>',
+        '  <h5>Elit</h5>',
+        '  <h5>Elit </h5>',
+        '  <p>sed</p>',
+        '</section>'
+      ].join('\n'),
+      (error, file) => {
+        t.ifErr(error, 'shouldn’t throw')
+
+        t.equal(
+          String(file),
+          [
+            '<section>',
+            '  <h1 id="test--Lorem ipsum 😪--test">Lorem ipsum 😪</h1>',
+            '  <h2 id="test--dolor—sit—amet--test">dolor—sit—amet</h2>',
+            '  <h3 id="test--consectetur &#x26; adipisicing--test">consectetur &#x26; adipisicing</h3>',
+            '  <h4 id="test--elit--test">elit</h4>',
+            '  <h5 id="test--Elit--test">Elit</h5>',
+            '  <h5 id="test--Elit --test">Elit </h5>',
+            '  <p>sed</p>',
+            '</section>'
+          ].join('\n'),
+          'should match'
+        )
+      }
+    )
+
+  rehype()
+    .data('settings', {fragment: true})
+    .use(rehypeSlug, {
+      prefix: 'prefix--',
+      slugger: {
+        slug: (_value) =>
+          `test--${_value.replace(/\W/g, '').toLowerCase()}--test`
+      }
+    })
+    .process(
+      [
+        '<section>',
+        '  <h1>Lorem ipsum 😪</h1>',
+        '  <h2>dolor—sit—amet</h2>',
+        '  <h3>consectetur &amp; adipisicing</h3>',
+        '  <h4>elit</h4>',
+        '  <h5>Elit</h5>',
+        '  <h5>Elit </h5>',
+        '  <p>sed</p>',
+        '</section>'
+      ].join('\n'),
+      (error, file) => {
+        t.ifErr(error, 'shouldn’t throw')
+
+        t.equal(
+          String(file),
+          [
+            '<section>',
+            '  <h1 id="prefix--test--loremipsum--test">Lorem ipsum 😪</h1>',
+            '  <h2 id="prefix--test--dolorsitamet--test">dolor—sit—amet</h2>',
+            '  <h3 id="prefix--test--consecteturadipisicing--test">consectetur &#x26; adipisicing</h3>',
+            '  <h4 id="prefix--test--elit--test">elit</h4>',
+            '  <h5 id="prefix--test--elit--test">Elit</h5>',
+            '  <h5 id="prefix--test--elit--test">Elit </h5>',
+            '  <p>sed</p>',
+            '</section>'
+          ].join('\n'),
+          'should match'
+        )
+      }
+    )
+
+  rehype()
+    .data('settings', {fragment: true})
+    .use(rehypeSlug, {
+      prefix: 'prefix--',
+      slugger: {
+        slug: (_value) =>
+          slugify(_value, {lower: true, strict: true, replacement: '__'})
+      }
+    })
+    .process(
+      [
+        '<section>',
+        '  <h1>Lorem ipsum 😪</h1>',
+        '  <h2>dolor—sit—amet</h2>',
+        '  <h3>consectetur &amp; adipisicing</h3>',
+        '  <h4>elit</h4>',
+        '  <h5>Elit</h5>',
+        '  <h5>Elit </h5>',
+        '  <p>sed</p>',
+        '</section>'
+      ].join('\n'),
+      (error, file) => {
+        t.ifErr(error, 'shouldn’t throw')
+
+        t.equal(
+          String(file),
+          [
+            '<section>',
+            '  <h1 id="prefix--lorem__ipsum">Lorem ipsum 😪</h1>',
+            '  <h2 id="prefix--dolorsitamet">dolor—sit—amet</h2>',
+            '  <h3 id="prefix--consectetur__and__adipisicing">consectetur &#x26; adipisicing</h3>',
+            '  <h4 id="prefix--elit">elit</h4>',
+            '  <h5 id="prefix--elit">Elit</h5>',
+            '  <h5 id="prefix--elit">Elit </h5>',
             '  <p>sed</p>',
             '</section>'
           ].join('\n'),
