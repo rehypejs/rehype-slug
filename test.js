22f1ee72556c486dd02cccaeedb3fec73cfdb2f8
Titus Wormer <tituswormer@gmail.com>
2023-08-31 15:54:39 +0200

    Refactor to use `node:test`

--- test.js
+++ test.js
@@ -1,75 +1,70 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {rehype} from 'rehype'
 import rehypeSlug from './index.js'
 
-test('rehypeSlug', (t) => {
-  t.plan(4)
+test('rehypeSlug', async function (t) {
+  await t.test('should work', async function () {
+    const file = await rehype()
+      .data('settings', {fragment: true})
+      .use(rehypeSlug)
+      .process(
+        [
+          '<section>',
+          '  <h1>Lorem ipsum 😪</h1>',
+          '  <h2>dolor—sit—amet</h2>',
+          '  <h3>consectetur &amp; adipisicing</h3>',
+          '  <h4>elit</h4>',
+          '  <h5>elit</h5>',
+          '  <p>sed</p>',
+          '</section>'
+        ].join('\n')
+      )
 
-  rehype()
-    .data('settings', {fragment: true})
-    .use(rehypeSlug)
-    .process(
+    assert.equal(
+      String(file),
       [
         '<section>',
-        '  <h1>Lorem ipsum 😪</h1>',
-        '  <h2>dolor—sit—amet</h2>',
-        '  <h3>consectetur &amp; adipisicing</h3>',
-        '  <h4>elit</h4>',
-        '  <h5>elit</h5>',
+        '  <h1 id="lorem-ipsum-">Lorem ipsum 😪</h1>',
+        '  <h2 id="dolorsitamet">dolor—sit—amet</h2>',
+        '  <h3 id="consectetur--adipisicing">consectetur &#x26; adipisicing</h3>',
+        '  <h4 id="elit">elit</h4>',
+        '  <h5 id="elit-1">elit</h5>',
         '  <p>sed</p>',
         '</section>'
-      ].join('\n'),
-      (error, file) => {
-        t.ifErr(error, 'shouldn’t throw')
-
-        t.equal(
-          String(file),
-          [
-            '<section>',
-            '  <h1 id="lorem-ipsum-">Lorem ipsum 😪</h1>',
-            '  <h2 id="dolorsitamet">dolor—sit—amet</h2>',
-            '  <h3 id="consectetur--adipisicing">consectetur &#x26; adipisicing</h3>',
-            '  <h4 id="elit">elit</h4>',
-            '  <h5 id="elit-1">elit</h5>',
-            '  <p>sed</p>',
-            '</section>'
-          ].join('\n'),
-          'should match'
-        )
-      }
+      ].join('\n')
     )
+  })
 
-  rehype()
-    .data('settings', {fragment: true})
-    .use(rehypeSlug, {prefix: 'test-'})
-    .process(
+  await t.test('should support `options.prefix`', async function () {
+    const file = await rehype()
+      .data('settings', {fragment: true})
+      .use(rehypeSlug, {prefix: 'test-'})
+      .process(
+        [
+          '<section>',
+          '  <h1>Lorem ipsum 😪</h1>',
+          '  <h2>dolor—sit—amet</h2>',
+          '  <h3>consectetur &amp; adipisicing</h3>',
+          '  <h4>elit</h4>',
+          '  <h5>elit</h5>',
+          '  <p>sed</p>',
+          '</section>'
+        ].join('\n')
+      )
+
+    assert.equal(
+      String(file),
       [
         '<section>',
-        '  <h1>Lorem ipsum 😪</h1>',
-        '  <h2>dolor—sit—amet</h2>',
-        '  <h3>consectetur &amp; adipisicing</h3>',
-        '  <h4>elit</h4>',
-        '  <h5>elit</h5>',
+        '  <h1 id="test-lorem-ipsum-">Lorem ipsum 😪</h1>',
+        '  <h2 id="test-dolorsitamet">dolor—sit—amet</h2>',
+        '  <h3 id="test-consectetur--adipisicing">consectetur &#x26; adipisicing</h3>',
+        '  <h4 id="test-elit">elit</h4>',
+        '  <h5 id="test-elit-1">elit</h5>',
         '  <p>sed</p>',
         '</section>'
-      ].join('\n'),
-      (error, file) => {
-        t.ifErr(error, 'shouldn’t throw')
-
-        t.equal(
-          String(file),
-          [
-            '<section>',
-            '  <h1 id="test-lorem-ipsum-">Lorem ipsum 😪</h1>',
-            '  <h2 id="test-dolorsitamet">dolor—sit—amet</h2>',
-            '  <h3 id="test-consectetur--adipisicing">consectetur &#x26; adipisicing</h3>',
-            '  <h4 id="test-elit">elit</h4>',
-            '  <h5 id="test-elit-1">elit</h5>',
-            '  <p>sed</p>',
-            '</section>'
-          ].join('\n'),
-          'should match'
-        )
-      }
+      ].join('\n')
     )
+  })
 })
